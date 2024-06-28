454b1bb55900cb5fc90a972cc486082fa9f37425
Abhay Luva <abhay.luva@mnhsoftskills.com>
2024-06-28 19:57:53 +0530

    chaneg

--- routes/web.php
+++ routes/web.php
@@ -37,12 +37,12 @@ Route::middleware('signin-check')->group(function () {
 
     /* User Category And Roles */
     Route::prefix('category')->group(function () {
-        Route::get('create', [CategoryController::class, 'create'])->name('category.create')->middleware(['role_or_permission:category-create']);
-        Route::post('store', [CategoryController::class, 'store'])->name('category.store')->middleware(['role_or_permission:category-create']);
-        Route::get('list', [CategoryController::class, 'index'])->name('category.list')->middleware(['role_or_permission:category-read']);
-        Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('category.edit')->middleware(['role_or_permission:category-update']);
-        Route::post('update/{id}', [CategoryController::class, 'update'])->name('category.update')->middleware(['role_or_permission:category-update']);
-        Route::get('status/{id}', [CategoryController::class, 'status'])->name('category.status')->middleware(['role_or_permission:category-status']);
+        Route::get('create', [CategoryController::class, 'create'])->name('category.create');
+        Route::post('store', [CategoryController::class, 'store'])->name('category.store');
+        Route::get('list', [CategoryController::class, 'index'])->name('category.list');
+        Route::get('edit/{id}', [CategoryController::class, 'edit'])->name('category.edit');
+        Route::post('update/{id}', [CategoryController::class, 'update'])->name('category.update');
+        Route::get('status/{id}', [CategoryController::class, 'status'])->name('category.status');
     });
     /* System Users */
     Route::prefix('user')->group(function () {

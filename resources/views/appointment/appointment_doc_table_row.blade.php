59e6d5fc92f52e7768f070e90203c7124ae88f55
Abhay Luva <abhay.luva@mnhsoftskills.com>
2024-12-23 20:08:01 +0530

    changes

--- resources/views/appointment/appointment_doc_table_row.blade.php
+++ resources/views/appointment/appointment_doc_table_row.blade.php
@@ -2,7 +2,7 @@
     <td>{{ $docData->ap_doc_name }}</td>
     <td>
         <!-- <a href="{{ route('appointment.doc.download', ['id' => base64_encode($docData->id)]) }}" download>{{ json_decode($docData->ap_doc)[0] }}</a> -->
-        <a href="{{ url('/') . '/public/storage/'.json_decode($docData->ap_doc)[0] }}" target="_blank">{{ json_decode($docData->ap_doc)[0] }}</a>
+        <a href="{{ url('/') . '/storage/'.json_decode($docData->ap_doc)[0] }}" target="_blank">{{ json_decode($docData->ap_doc)[0] }}</a>
     </td>
     <td>
         <i title="Remove" class="la la-trash icon-3x cursor_pointer" onclick="removerDoc('{{ $docData->id }}')"></i>

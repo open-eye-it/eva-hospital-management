d9729e5f359495f2ba032fbf2c165ea28cfedad3
Abhay Luva <abhay.luva@mnhsoftskills.com>
2024-12-28 20:18:28 +0530

    change

--- resources/views/ipd/bill_print.blade.php
+++ resources/views/ipd/bill_print.blade.php
@@ -48,7 +48,7 @@
         <table style="margin:auto; width:100%; padding:5px; margin-top:20px;">
             <tr>
                 <td style="font-weight: bold;">Age/Gender</td>
-                <td style="width:40%;">{{ $data1['age'] }}</td>
+                <td style="width:40%;">{{ $data1['age'] }} Yrs/{{ ucfirst(substr($data1['gender'], 0, 1)) }}</td>
                 <td style="font-weight: bold;">Admission Date/Time</td>
                 <td>{{ date('d M Y, h:i a', strtotime($data1['ipd_admit_date'])) }}</td>
             </tr>

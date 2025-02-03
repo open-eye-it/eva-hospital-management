0874f7b96e7e3f62429b1bbab3615a7bd7b64bff
Abhay Luva <abhay.luva@mnhsoftskills.com>
2025-02-03 08:20:50 +0530

    change

--- resources/views/appointment/prescription_print.blade.php
+++ resources/views/appointment/prescription_print.blade.php
@@ -49,32 +49,32 @@
 
                             <table style="width:100%;">
                                 <tr>
-                                    <td style="padding:10px;" colspan="2"><strong>Bill No: </strong>{{ $data->ap_id }}</td>
-                                    <td style="padding:10px;" colspan="2"><strong>Patient ID: </strong>{{ $data->pa_id }}</td>
-                                    <td style="padding:10px;" colspan="2"><strong>Date: </strong>{{ date('d M Y') }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Appointment ID: </strong>{{ $data->ap_id }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Patient ID: </strong>{{ $data->pa_id }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Date: </strong>{{ date('d M Y') }}</td>
                                 </tr>
                                 <tr>
-                                    <td style="padding:10px;" colspan="4"><strong>Patient Name: </strong>{{ $data->patientData->pa_name }}</td>
-                                    <td style="padding:10px;" colspan="2"><strong>Age: </strong>{{ $data->patientData->pa_age }}</td>
+                                    <td style="padding:5px;" colspan="4"><strong>Patient Name: </strong>{{ $data->patientData->pa_name }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Age: </strong>{{ $data->patientData->pa_age }}</td>
                                 </tr>
                                 <tr>
-                                    <td style="padding:10px;" colspan="6"><strong>Address: </strong>{{ $data->patientData->pa_address.' '.$data->patientData->pa_city.' '.$data->patientData->pa_pincode.' '.$data->patientData->pa_state }}</td>
+                                    <td style="padding:5px;" colspan="6"><strong>Address: </strong>{{ $data->patientData->pa_address.' '.$data->patientData->pa_city.' '.$data->patientData->pa_pincode.' '.$data->patientData->pa_state }}</td>
                                 </tr>
                                 <tr>
-                                    <td style="padding:10px;" colspan="2"><strong>Doctor: </strong>{{ $data->doctorData->person_name }}</td>
-                                    <td style="padding:10px;" colspan="2"><strong>Referred By</strong>{{ ($data->patientData->pa_referred_by == 'doctor') ? $data->patientData->pa_referred_doctor : $data->patientData->pa_referred_text }}</td>
-                                    <td style="padding:10px;" colspan="2"><strong>Follow Up Date: </strong>{{ ($data->ap_follow_up_date != null && $data->ap_follow_up_date != '0000-00-00') ? date('d M Y', strtotime($data->ap_follow_up_date)) : '' }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Doctor: </strong>{{ $data->doctorData->person_name }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Referred By</strong>{{ ($data->patientData->pa_referred_by == 'doctor') ? $data->patientData->pa_referred_doctor : $data->patientData->pa_referred_text }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Follow Up Date: </strong>{{ ($data->ap_follow_up_date != null && $data->ap_follow_up_date != '0000-00-00') ? date('d M Y', strtotime($data->ap_follow_up_date)) : '' }}</td>
                                 </tr>
                                 <tr>
-                                    <td style="padding:10px;" colspan="2"><strong>Height: </strong>{{ $data->ap_height }}</td>
-                                    <td style="padding:10px;" colspan="2"><strong>Weight: </strong>{{ $data->ap_weight }}</td>
-                                    <td style="padding:10px;" colspan="2"><Strong>BP: </Strong>{{ $data->ap_bp }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Height: </strong>{{ $data->ap_height }}</td>
+                                    <td style="padding:5px;" colspan="2"><strong>Weight: </strong>{{ $data->ap_weight }}</td>
+                                    <td style="padding:5px;" colspan="2"><Strong>BP: </Strong>{{ $data->ap_bp }}</td>
                                 </tr>
                                 <tr>
-                                    <td style="padding:10px;" colspan="6"><Strong>Complaints: </Strong>{{ $data->ap_complaint }}</td>
+                                    <td style="padding:5px;" colspan="6"><Strong>Complaints: </Strong>{{ $data->ap_complaint }}</td>
                                 </tr>
                                 <tr>
-                                    <td style="padding:10px;" colspan="6"><strong>Other Details: </strong>{{ $data->ap_other_detail }}</td>
+                                    <td style="padding:5px;" colspan="6"><strong>Other Details: </strong>{{ $data->ap_other_detail }}</td>
                                 </tr>
                                 <tr>
                                     <td colspan="6">
@@ -84,41 +84,41 @@
                                     </td>
                                 </tr>
                                 <tr>
-                                    <th style="padding:10px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">Medicine Name</th>
-                                    <th style="padding:10px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">No. of Days</th>
-                                    <th style="padding:10px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">Timing</th>
-                                    <th style="padding:10px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">Morning</th>
-                                    <th style="padding:10px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">Afternoon</th>
-                                    <th style="padding:10px; border-top:1px solid; border-bottom:1px solid; text-align:left;">Evening</th>
+                                    <th style="padding:5px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">Medicine Name</th>
+                                    <th style="padding:5px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">No. of Days</th>
+                                    <th style="padding:5px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">Timing</th>
+                                    <th style="padding:5px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">Morning</th>
+                                    <th style="padding:5px; border-top:1px solid; border-bottom:1px solid; border-right:1px solid; text-align:left;">Afternoon</th>
+                                    <th style="padding:5px; border-top:1px solid; border-bottom:1px solid; text-align:left;">Evening</th>
                                 </tr>
                                 @if(!empty($prescribeMedicineList->toArray()))
                                 @foreach($prescribeMedicineList as $prescribeMedicine)
                                 <tr>
-                                    <td style="padding:10px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->medicineData->gm_name.' ('.$prescribeMedicine->medicineData->gm_company_name.')' }}</td>
-                                    <td style="padding:10px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->am_days }}</td>
-                                    <td style="padding:10px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->am_timing }}</td>
-                                    <td style="padding:10px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->am_morning }}</td>
-                                    <td style="padding:10px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->am_afternoon }}</td>
-                                    <td style="padding:10px; border-bottom:1px solid;">{{ $prescribeMedicine->am_evening }}</td>
+                                    <td style="padding:5px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->medicineData->gm_name.' ('.$prescribeMedicine->medicineData->gm_company_name.')' }}</td>
+                                    <td style="padding:5px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->am_days }}</td>
+                                    <td style="padding:5px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->am_timing }}</td>
+                                    <td style="padding:5px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->am_morning }}</td>
+                                    <td style="padding:5px; border-bottom:1px solid; border-right:1px solid;">{{ $prescribeMedicine->am_afternoon }}</td>
+                                    <td style="padding:5px; border-bottom:1px solid;">{{ $prescribeMedicine->am_evening }}</td>
                                 </tr>
                                 @endforeach
                                 @endif
                                 <tr>
-                                    <td style="padding:10px;" colspan="6"><Strong>Any Advice: </Strong>{{ $data->ap_any_advice }}</td>
+                                    <td style="padding:5px;" colspan="6"><Strong>Any Advice: </Strong>{{ $data->ap_any_advice }}</td>
                                 </tr>
                                 @if($data->ap_surg_required == 'yes')
                                 <tr>
-                                    <td style="padding:10px;" colspan="2"><Strong>Surgery Required?: </Strong>{{ ($data->ap_surg_required == 'yes') ? 'Yes' : 'No' }}</td>
-                                    <td style="padding:10px;" colspan="4"><strong>Surgery Date: </strong>{{ ($data->ap_surg_date != null && $data->ap_surg_date != '0000-00-00') ? date('d M Y', strtotime($data->ap_surg_date)) : '' }}</td>
+                                    <td style="padding:5px;" colspan="2"><Strong>Surgery Required?: </Strong>{{ ($data->ap_surg_required == 'yes') ? 'Yes' : 'No' }}</td>
+                                    <td style="padding:5px;" colspan="4"><strong>Surgery Date: </strong>{{ ($data->ap_surg_date != null && $data->ap_surg_date != '0000-00-00') ? date('d M Y', strtotime($data->ap_surg_date)) : '' }}</td>
                                 </tr>
                                 <tr>
-                                    <td style="padding:10px;" colspan="2"><Strong>Type of Surgery: </Strong>{{ $data->ap_surg_type }}</td>
-                                    <td style="padding:10px;" colspan="4"><strong>Is FOC?: </strong>{{ ($data->ap_is_foc == 'yes') ? 'Yes' : 'No' }}</td>
+                                    <td style="padding:5px;" colspan="2"><Strong>Type of Surgery: </Strong>{{ $data->ap_surg_type }}</td>
+                                    <td style="padding:5px;" colspan="4"><strong>Is FOC?: </strong>{{ ($data->ap_is_foc == 'yes') ? 'Yes' : 'No' }}</td>
                                 </tr>
                                 @else
                                 <tr>
-                                    <td style="padding:10px;" colspan="2"><Strong>Surgery Required?: </Strong>{{ ($data->ap_surg_required == 'yes') ? 'Yes' : 'No' }}</td>
-                                    <td style="padding:10px;" colspan="4"><strong>Is FOC?: </strong>{{ ($data->ap_is_foc == 'yes') ? 'Yes' : 'No' }}</td>
+                                    <td style="padding:5px;" colspan="2"><Strong>Surgery Required?: </Strong>{{ ($data->ap_surg_required == 'yes') ? 'Yes' : 'No' }}</td>
+                                    <td style="padding:5px;" colspan="4"><strong>Is FOC?: </strong>{{ ($data->ap_is_foc == 'yes') ? 'Yes' : 'No' }}</td>
                                 </tr>
                                 @endif
                             </table>
